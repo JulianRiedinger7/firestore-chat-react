0b9ab7c9fb1b784701baf9f730cf7cdef144f4d6
Hernan <hernansantarelli87@gmail.com>
2022-10-24 16:21:35 -0300

    segundo commit de hernan

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ function App() {
 	return (
 		<div>
 			<h1 className="text-5xl text-red-500">hola juliiii testing</h1>
+			<h1 className="text-5xl text-red-500">hola hernaaaaan testing</h1>
 		</div>
 	);
 }
